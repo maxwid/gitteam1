f1c5b93c8bd53329768f73424e660bcbc9d297ad
ptorngh <patrik.tornberg@hm.com>
2021-02-05 13:49:01 +0100

    new method addition

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int input1 = 3;
+        int input2 = 5;
+        int result = addition(input1,input2);
+        System.out.println(input1 + " * " + input2 + " = " + result);
+    }
+
+    public static int addition(int a, int b) {
+        return (a*b);
     }
 }
