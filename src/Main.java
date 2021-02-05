51aeb4fb34b60ad617d7387f2adc6466b53cdc6e
iderbrandt <tobias.iderbrandt@hm.com>
2021-02-05 13:32:00 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+
+        System.out.println(+num1+ " + " +num2+ " = " + (num1+num2));
+        System.out.println(+num1+ " - " +num2+ " = " + (num1-num2));
+        System.out.println(+num1+ " * " +num2+ " = " + (num1*num2));
+        System.out.println(+num1+ " / " +num2+ " = " + (1.0*num1/num2));
     }
 }
