7daef8dd6c4da14e0c35af9420f3afda4c779b6b
iderbrandt <tobias.iderbrandt@hm.com>
2021-02-05 14:39:29 +0100

    Merge

--- src/Main.java
+++ src/Main.java
@@ -16,31 +16,28 @@ public class Main {
         System.out.println("The Result");
         System.out.println(+num1+ " * " +num2+ " = " + (multi(num1,num2)));
 
-        }
-
-        public static int multi (int num1, int num2) {
-            return num1*num2;
-        }
-    }
-
         int input1 = 3;
         int input2 = 5;
         int numberToSubtractFrom = 75;
         int numberToSubtract = 50;
 
-        int result1 = addition(input1,input2);
+        int result1 = addition(input1, input2);
         int result = subtract(numberToSubtract, numberToSubtractFrom);
         System.out.println(input1 + " + " + input2 + " = " + result1);
         System.out.println("This is the result: " + result);
-    }
 
-    public static int subtract(int numToSub, int numFrom) {
+        }
+
+        public static int multi(int num1, int num2) {
+        return num1 * num2;
+        }
+
+        public static int subtract(int numToSub, int numFrom) {
         return numFrom-numToSub;
+        }
 
-
-    }
-
-    public static int addition(int a, int b) {
+        public static int addition(int a, int b) {
         return (a+b);
     }
 }
+
