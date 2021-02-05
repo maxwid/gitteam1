89f8f74544bc1ce547fcea19d9de6a8534c81532
maxwid <max.widell@hm.com>
2021-02-05 13:40:16 +0100

    Added function for Subtraction

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int numberToSubtractFrom = 75;
+        int numberToSubtract = 50;
+
+        int result = subtract(numberToSubtract, numberToSubtractFrom);
+
+        System.out.println("This is the result: " + result);
+    }
+
+    public static int subtract(int numToSub, int numFrom) {
+        return numFrom-numToSub;
     }
 }
